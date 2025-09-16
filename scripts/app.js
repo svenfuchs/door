0dccc43c33874678467aa6c1a7f249b2c8810d4d
Sven Fuchs <me@svenfuchs.com>
2025-09-16 15:59:03 +0200

    add shareable code

--- scripts/app.js
+++ scripts/app.js
@@ -13,6 +13,7 @@ const input = {
   lgt: document.getElementsByName('lgt')[0],
   accent: document.getElementsByName('accent')[0],
   element: document.getElementsByName('element')[0],
+  code: document.getElementById('code'),
 };
 
 var state = {
@@ -51,6 +52,12 @@ function toggleElement() {
   update();
 }
 
+function codeChange(e) {
+  loadCode(e.value);
+  setSliders();
+  update();
+};
+
 function setSliders() {
   input.hue.value = filter[state.element].hue;
   input.sat.value = filter[state.element].sat;
@@ -62,6 +69,7 @@ input.sat.addEventListener('input', sliderChange);
 input.lgt.addEventListener('input', sliderChange);
 input.accent.addEventListener('input', toggleAccent);
 input.element.addEventListener('input', toggleElement);
+input.code.addEventListener('change', codeChange);
 
 function update() {
   updateElement('door');
@@ -71,6 +79,7 @@ function update() {
   element.elLabel.innerText = state.element == 'door' ? 'Tür' : 'Akzent';
 
   storeQuery();
+  storeCode();
 }
 
 function updateElement(el) {
@@ -83,12 +92,14 @@ function updateElement(el) {
 
 function loadQuery() {
   const url = new URL(window.location.href);
-  filter.door.hue = url.searchParams.get('door.hue');
-  filter.door.sat = url.searchParams.get('door.sat');
-  filter.door.lgt = url.searchParams.get('door.lgt');
-  filter.accent.hue = url.searchParams.get('accent.hue');
-  filter.accent.sat = url.searchParams.get('accent.sat');
-  filter.accent.lgt = url.searchParams.get('accent.lgt');
+  filter.door.hue = url.searchParams.get('door.hue') | filter.door.hue;
+  filter.door.sat = url.searchParams.get('door.sat') | filter.door.sat;
+  filter.door.lgt = url.searchParams.get('door.lgt') | filter.door.lgt;
+  if(state.accent) {
+    filter.accent.hue = url.searchParams.get('accent.hue') | filter.accent.hue;
+    filter.accent.sat = url.searchParams.get('accent.sat') | filter.accent.sat;;
+    filter.accent.lgt = url.searchParams.get('accent.lgt') | filter.accent.lgt;;
+  }
   replaceState(null, '', url.toString());
 }
 
@@ -97,13 +108,44 @@ function storeQuery() {
   url.searchParams.set('door.hue', filter.door.hue);
   url.searchParams.set('door.sat', filter.door.sat);
   url.searchParams.set('door.lgt', filter.door.lgt);
-  url.searchParams.set('accent.hue', filter.accent.hue);
-  url.searchParams.set('accent.sat', filter.accent.sat);
-  url.searchParams.set('accent.lgt', filter.accent.lgt);
+  if(state.accent) {
+    url.searchParams.set('accent.hue', filter.accent.hue);
+    url.searchParams.set('accent.sat', filter.accent.sat);
+    url.searchParams.set('accent.lgt', filter.accent.lgt);
+  } else {
+    url.searchParams.delete('accent.hue');
+    url.searchParams.delete('accent.sat');
+    url.searchParams.delete('accent.lgt');
+  }
   replaceState(null, '', url.toString());
 }
 
-function panImage() {
+function storeCode() {
+  var data = Object.values(filter.door);
+  if (state.accent) {
+    data = data.concat(Object.values(filter.accent));
+  }
+  code.value = data.join(',');
+}
+
+function loadCode() {
+  var code = input.code.value;
+  var data = code.split(',');
+
+  if (data.length != 3 && data.length != 6) {
+    console.error("Invalid code");
+    return;
+  }
+
+  filter.door.hue = data[0];
+  filter.door.sat = data[1];
+  filter.door.lgt = data[2];
+  filter.accent.hue = data[3];
+  filter.accent.sat = data[4];
+  filter.accent.lgt = data[5];
+}
+
+function initImage() {
   var width = element.imgs.scrollWidth;
   var height = element.imgs.scrollHeight;
 
@@ -112,11 +154,14 @@ function panImage() {
 
   element.imgs.style.top = `-${overY / 2}px`;
   element.imgs.style.left = `-${overX / 2}px`;
+
+  if(width < window.innerWidth) {
+  }
 }
 
-window.onload = function() {
+window.addEventListener("load", () => {
   loadQuery();
   setSliders();
   update();
-  panImage();
-}
+  initImage();
+});
--- scripts/touch.js
+++ scripts/touch.js
@@ -1,59 +1,71 @@
-let scale = 1;
-let lastScale = 1;
-let posX = 0;
-let posY = 0;
-let lastX = 0;
-let lastY = 0;
-let dragging = false;
+window.addEventListener("load", () => {
+  let scale = 1;
+  let lastScale = 1;
+  let posX = 0;
+  let posY = 0;
+  let lastX = 0;
+  let lastY = 0;
+  // let origX = 0;
+  // let origY = 0;
+  let dragging = false;
 
-var imgs = element.imgs;
+  var body = document.body;
+  var imgs = document.getElementById('imgs');
 
-function updateTransform() {
-  imgs.style.transform = `translate(${posX}px, ${posY}px) scale(${scale})`;
-}
-
-imgs.addEventListener('gesturestart', e => {
-  e.preventDefault();
-  lastScale = scale;
-});
-
-imgs.addEventListener('gesturechange', e => {
-  e.preventDefault();
-  scale = Math.max(1, Math.min(lastScale * e.scale, 5)); // clamp zoom between 1x and 5x
-  updateTransform();
-});
-
-imgs.addEventListener('gestureend', e => {
-  e.preventDefault();
-  lastScale = scale;
-});
-
-imgs.addEventListener('touchstart', e => {
-  if (e.touches.length === 1) {
-    dragging = true;
-    lastX = e.touches[0].clientX - posX;
-    lastY = e.touches[0].clientY - posY;
+  function updateTransform() {
+    // imgs.style.transformOrigin = `${origX}% ${origY}%`
+    imgs.style.transform = `translate(${posX}px, ${posY}px) scale(${scale})`;
   }
-});
 
-imgs.addEventListener('touchmove', e => {
-  if (dragging && e.touches.length === 1) {
+  body.addEventListener('gesturestart', e => {
     e.preventDefault();
-    posX = e.touches[0].clientX - lastX;
-    posY = e.touches[0].clientY - lastY;
+    lastScale = scale;
+  });
+
+  body.addEventListener('gesturechange', e => {
+    e.preventDefault();
+    // origX = e.offsetX * 100 / imgs.offsetWidth;
+    // origY = e.offsetY * 100 / imgs.offsetHeight;
+    scale = Math.max(0.1, Math.min(lastScale * e.scale, 5)); // clamp zoom between 0.1x and 5x
     updateTransform();
-  }
-});
+  });
 
-imgs.addEventListener('touchend', e => {
-  if (e.touches.length === 0) {
-    dragging = false;
-  }
-});
+  body.addEventListener('gestureend', e => {
+    e.preventDefault();
+    lastScale = scale;
+  });
 
-imgs.addEventListener("wheel", e => {
-  e.preventDefault();
-  posX -= e.deltaX;
-  posY -= e.deltaY;
-  updateTransform();
+  body.addEventListener('touchstart', e => {
+    e.preventDefault();
+    if (e.touches.length === 1) {
+      dragging = true;
+      lastX = e.touches[0].clientX - posX;
+      lastY = e.touches[0].clientY - posY;
+    }
+    return false;
+  });
+
+  body.addEventListener('touchmove', e => {
+    e.preventDefault();
+    if (dragging && e.touches.length === 1) {
+      posX = e.touches[0].clientX - lastX;
+      posY = e.touches[0].clientY - lastY;
+      updateTransform();
+    }
+    return false;
+  });
+
+  body.addEventListener('touchend', e => {
+    e.preventDefault();
+    if (e.touches.length === 0) {
+      dragging = false;
+    }
+  });
+
+  body.addEventListener("wheel", e => {
+    e.preventDefault();
+    posX -= e.deltaX;
+    posY -= e.deltaY;
+    updateTransform();
+  });
 });
